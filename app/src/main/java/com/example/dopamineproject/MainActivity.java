999f8c07d6fa4a7d797e0293477431d754942f84
Geet99 <17ucs059>
2019-10-20 21:44:42 +0530

    song added

--- app/src/main/java/com/example/dopamineproject/MainActivity.java
+++ app/src/main/java/com/example/dopamineproject/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.dopamineproject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.song);
+        Button music = (Button) this.findViewById(R.id.music);
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
     }
 
     public void trackmood(View view ){
