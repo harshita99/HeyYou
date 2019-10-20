0b8603019a25ba522a496e7d67e6d2d5dc1dc1e5
Geet99 <17ucs059>
2019-10-20 16:56:41 +0530

    geet committed

---
+++ app/src/main/java/com/example/dopamineproject/DrawingPad.java
@@ -0,0 +1,23 @@
+package com.example.dopamineproject;
+
+import android.graphics.Path;
+
+public class DrawingPad {
+
+    public int color;
+    public boolean emboss;
+    public boolean blur;
+    public int strokeWidth;
+    public Path path;
+
+    public DrawingPad(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
+    }
+
+
+
+}
